3522243237813366214a6f96124d2d6c216162c5
Javier Ortiz <javier.ortiz989@gmail.com>
2021-10-20 13:52:10 -0700

    dataset

---
+++ week9/wednesday/data-in-elements/starter/data.js
@@ -0,0 +1,31 @@
+// when the user clicks the button, prevent the default behavior of buttons inside a form
+
+// select the name inside the input#name
+
+// select the category inside the select#type
+
+// create an Li element
+/*
+    <li></li>
+*/
+
+// Add the name as the inner text
+/*
+<li>Margarita</li>
+*/
+
+// Add an attribute of data-type equal to the category
+/*
+<li data-type='Drinks'>Margarita</li>
+*/
+
+// append it to the ul
+/*
+<ul>
+    <li data-type='Drinks'>Margarita</li>
+</ul>
+*/
+
+// Delete the value from the input so that the user can enter a new one
+
+// bonus, style the li based on its data-type
